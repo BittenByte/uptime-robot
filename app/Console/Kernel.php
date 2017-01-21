5d2ddb976616312b7a400c71a0ff42bcf4051e41
Jose H. Milan <jhmilan@gmail.com>
2017-01-21 13:00:03 +0100

    Initial commit

---
+++ app/Console/Kernel.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace BittenByteUptimeMonitor\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        //
+    ];
+
+    /**
+     * Define the application's command schedule.
+     *
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        $schedule->command('monitor:check-uptime')->everyMinute();
+        $schedule->command('monitor:check-certificate')->daily();
+    }
+
+    /**
+     * Register the Closure based commands for the application.
+     */
+    protected function commands()
+    {
+        require base_path('routes/console.php');
+    }
+}
